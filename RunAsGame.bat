9ffdfa31194e4187fe2cd0e4961e7a8d8f2455e2
AbsoluteMikhail <ruage@vk.com>
2025-07-17 23:05:55 +0300

    Запускатор

---
+++ RunAsGame.bat
@@ -0,0 +1,4 @@
+:: укажи место установки движка и место расположения проекта
+:: не забудь про расширение
+:: обрати внимание в UE4 UE4Editor.exe а в UE5 UnrealEditor.exe
+"U:\UNREAL_ENGINE\UE_5.6\Engine\Binaries\Win64\UnrealEditor.exe" "U:\UnrealProjects\Zero\Zero.uproject" -game -ResX=1280 -RexY=720 -WinX=0 -log
